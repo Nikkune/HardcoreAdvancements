ecd58c486802c3e0780ada9ae4c0b30ea49e0d1b
Nikkune Devs <nikkune.dev@protonmail.com>
2023-03-12 15:25:36 +0100

    Initial commit

---
+++ src/main/java/fr/nikkune/ha/HardcoreAdvancementsClient.java
@@ -0,0 +1,10 @@
+package fr.nikkune.ha;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardcoreAdvancementsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
